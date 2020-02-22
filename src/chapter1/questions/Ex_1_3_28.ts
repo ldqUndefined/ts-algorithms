2d0fc7b3889622493437d72b771c4155f1742f01
ldqUndefined <435499825@qq.com>
2020-02-23 02:32:03 +0800

    :memo: Exercise 1.3.28

---
+++ src/chapter1/questions/Ex_1_3_28.ts
@@ -0,0 +1,17 @@
+import { Node } from '../implements/P89_Node';
+
+export const max = (first: Node<number> | null): number => {
+  if (!first) {
+    return 0;
+  }
+  if (!first.next) {
+    return first.item;
+  }
+  return Math.max(first.item, max(first.next));
+};
+
+//test
+let a = new Node(78);
+let b = new Node(456, a);
+let c = new Node(12, b);
+console.log(max(c));
