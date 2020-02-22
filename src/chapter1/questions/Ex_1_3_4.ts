d27e88b6fbd5dc85388b2fccfb7be7ff6f5ea6ad
ldqUndefined <435499825@qq.com>
2020-02-22 23:37:04 +0800

    :memo: Exercise 1.3.4

---
+++ src/chapter1/questions/Ex_1_3_4.ts
@@ -0,0 +1,21 @@
+import { Stack } from '../implements/P94_Stack';
+
+const Parentheses = (s: string): boolean => {
+  const stack = new Stack<string>();
+  for (let i = 0, len = s.length; i < len; i++) {
+    if (s[i] === '(' || s[i] === '[' || s[i] === '{') {
+      stack.push(s[i]);
+    }
+    if (
+      (s[i] === ')' && (stack.isEmpty() || stack.pop() !== '(')) ||
+      (s[i] === '}' && (stack.isEmpty() || stack.pop() !== '{')) ||
+      (s[i] === ']' && (stack.isEmpty() || stack.pop() !== '['))
+    ) {
+      return false;
+    }
+  }
+  return true;
+};
+//test
+console.log(Parentheses('[()]{}{[()()]()}'));
+console.log(Parentheses('[(])'));
