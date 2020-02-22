63cf069cd6037fe825ece56fcd52eff281b8869c
ldqUndefined <435499825@qq.com>
2020-02-22 23:38:12 +0800

    :memo: Exercise 1.3.24

---
+++ src/chapter1/questions/Ex_1_3_24.ts
@@ -0,0 +1,8 @@
+import { Node } from '../implements/P89_Node';
+
+export const removeAfter = <T>(node: Node<T> | null) => {
+  if (!node || !node.next) {
+    return;
+  }
+  node.next = node.next.next;
+};
