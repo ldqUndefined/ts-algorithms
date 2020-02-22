2807b9268d58b8b54aeaa19ee7e3c99346715236
ldqUndefined <435499825@qq.com>
2020-02-23 01:56:08 +0800

    :sparkles: 给Node新增迭代器，方便测试

--- src/chapter1/implements/P89_Node.ts
+++ src/chapter1/implements/P89_Node.ts
@@ -1,8 +1,31 @@
+import { IteratorResult } from '../../Util';
 export class Node<T> {
   item: T;
   next: Node<T> | null;
   constructor(value: T, next: Node<T> | null = null) {
     this.item = value;
     this.next = next;
+  }
+  //搞个节点的迭代器，方便测试看每个节点的输出
+  private [Symbol.iterator]() {
+    //声明个虚拟的头结点，以this为下一个节点，方便判断
+    let iteratorNode = new Node<T>(null as any, this);
+    return {
+      next(): IteratorResult<T> {
+        if (iteratorNode.next) {
+          let value = iteratorNode.next.item;
+          iteratorNode = iteratorNode.next;
+          return {
+            value,
+            done: false
+          };
+        } else {
+          return {
+            value: undefined,
+            done: true
+          };
+        }
+      }
+    };
   }
 }
