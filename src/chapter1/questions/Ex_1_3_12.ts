344069044ce2eeb10df8e8a37dc9239e2cec46c2
ldqUndefined <435499825@qq.com>
2020-02-22 23:37:48 +0800

    :memo: Exercise 1.3.12

---
+++ src/chapter1/questions/Ex_1_3_12.ts
@@ -0,0 +1,17 @@
+import { Stack } from '../implements/P94_Stack';
+
+let a = new Stack<string>();
+
+a.push('hahh');
+a.push('fdsfsdafsda');
+a.push('35235235');
+
+// for (let s of a) {
+//   console.log(s);
+// }
+
+let b = Stack.copy(a);
+console.log(a === b);
+for (let s of b) {
+  console.log(s);
+}
