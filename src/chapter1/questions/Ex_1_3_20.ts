b6eb73df41dc3b0583bdedce1f453378fb45a9a9
ldqUndefined <435499825@qq.com>
2020-02-22 23:38:00 +0800

    :memo: Exercise 1.3.20

---
+++ src/chapter1/questions/Ex_1_3_20.ts
@@ -0,0 +1,31 @@
+import { Node } from '../implements/P89_Node';
+
+export const deleteK = <T>(
+  first: Node<T> | null,
+  k: number
+): Node<T> | null => {
+  if (!first) {
+    throw new Error('链表为空');
+  }
+  if (k == 1) {
+    return first.next;
+  }
+  let preK = first;
+  while (k-- > 2) {
+    preK = preK.next!;
+    if (!preK.next) {
+      throw new Error('k超出列表长度');
+    }
+  }
+  preK.next = preK.next!.next;
+  return first;
+};
+
+//test
+
+let a = new Node('1');
+let b = new Node('2', a);
+let c: Node<string> | null = new Node('3', b);
+
+c = deleteK(c, 1);
+console.log(c);
