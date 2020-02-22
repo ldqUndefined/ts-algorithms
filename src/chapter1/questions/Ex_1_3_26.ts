944f29c0f2695dda6179575d591cc844cdd2faac
ldqUndefined <435499825@qq.com>
2020-02-22 23:38:26 +0800

    :memo: Exercise 1.3.26

---
+++ src/chapter1/questions/Ex_1_3_26.ts
@@ -0,0 +1,23 @@
+import { Node } from '../implements/P89_Node';
+
+export const remove = <T>(first: Node<T> | null, key: T): Node<T> | null => {
+  //声明一个虚拟的头结点可以少判断很多关于头部是否存在问题，看别人博客学来的
+  let header = new Node<T>(null as any, first);
+  let current = header;
+  while (current.next) {
+    if (current.next.item === key) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+  return header.next;
+};
+
+//test
+let a = new Node('k');
+let b = new Node('j', a);
+let c: Node<string> | null = new Node('k', b);
+
+c = remove(c, 'k');
+console.log(c);
