4656ac7bd383e05f169c7154cecaecafdc717ff4
ldqUndefined <435499825@qq.com>
2020-02-23 21:31:39 +0800

    :memo: Exercise 1.3.35

---
+++ src/chapter1/questions/Ex_1_3_35.ts
@@ -0,0 +1,41 @@
+import { shuffle } from '../../Util';
+export class RandomQueue<T> {
+  private arr: T[];
+  constructor() {
+    this.arr = [];
+  }
+  isEmpty(): boolean {
+    return this.arr.length === 0;
+  }
+  enqueue(value: T) {
+    this.arr.push(value);
+  }
+  dequeue(): T {
+    if (this.arr.length === 0) {
+      throw new Error('队列为空');
+    }
+    let toChangeIndex = Math.floor(Math.random() * this.arr.length);
+    let result = this.arr[toChangeIndex];
+    this.arr[toChangeIndex] = this.arr.pop()!;
+    return result;
+  }
+  sample(): T {
+    let index = Math.floor(Math.random() * this.arr.length);
+    return this.arr[index];
+  }
+  //Exercise 1.3.36
+  private [Symbol.iterator]() {
+    let temp = [...this.arr];
+    temp = shuffle(temp);
+    return temp[Symbol.iterator]();
+  }
+}
+
+//test
+let q = new RandomQueue<number>();
+q.enqueue(1);
+q.enqueue(2);
+q.enqueue(3);
+for (let s of q) {
+  console.log(s);
+}
