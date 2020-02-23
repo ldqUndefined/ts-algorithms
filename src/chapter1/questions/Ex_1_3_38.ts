222bda8e48d544c84b77cebe0ddb7afa712304d5
ldqUndefined <435499825@qq.com>
2020-02-23 21:31:53 +0800

    :memo: Exercise 1.3.38

---
+++ src/chapter1/questions/Ex_1_3_38.ts
@@ -0,0 +1,51 @@
+import { Node } from '../implements/P89_Node';
+
+export class GeneralizedQueue<T> {
+  private head: Node<T> | null;
+  private length: number;
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+  isEmpty(): boolean {
+    return this.length === 0;
+  }
+  insert(value: T) {
+    let newNode = new Node(value, this.head);
+    this.head = newNode;
+    this.length++;
+  }
+  delete(k: number): T {
+    if (k > this.length) {
+      throw new Error('K大于队列长度');
+    }
+    if (!this.head) {
+      throw new Error('队列为空');
+    }
+    let cur = new Node(null as any, this.head);
+    while (k++ < this.length) {
+      cur = cur.next!;
+    }
+    let result = cur.next!.item;
+    if (cur.next === this.head) {
+      this.head = cur.next.next;
+    }
+    cur.next = cur.next!.next;
+    return result;
+  }
+  //搞个返回head方便测试
+  getHead() {
+    return this.head;
+  }
+}
+
+//test
+let q = new GeneralizedQueue<string>();
+q.insert('1');
+q.insert('2');
+q.insert('3');
+q.insert('4');
+q.delete(3);
+for (let s of q.getHead()!) {
+  console.log(s);
+}
