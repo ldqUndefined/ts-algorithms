6fc122fe4855c90f254c23c06e6fedd4acf449a4
ldqUndefined <435499825@qq.com>
2020-02-23 21:32:04 +0800

    :memo: Exercise 1.3.44

---
+++ src/chapter1/questions/Ex_1_3_44.ts
@@ -0,0 +1,33 @@
+import { Stack } from '../implements/P94_Stack';
+import { IteratorResult } from '../../Util';
+export class Buffer {
+  private cursorLeft: Stack<string>;
+  private cursorRight: Stack<string>;
+  constructor() {
+    this.cursorLeft = new Stack<string>();
+    this.cursorRight = new Stack<string>();
+  }
+  insert(s: string) {
+    this.cursorLeft.push(s);
+  }
+  delete(): string | undefined {
+    return this.cursorLeft.pop();
+  }
+  left(k: number) {
+    while (k-- > 0) {
+      if (!this.cursorLeft.isEmpty()) {
+        this.cursorRight.push(this.cursorLeft.pop()!);
+      }
+    }
+  }
+  right(k: number) {
+    while (k-- > 0) {
+      if (!this.cursorRight.isEmpty()) {
+        this.cursorLeft.push(this.cursorRight.pop()!);
+      }
+    }
+  }
+  size() {
+    return this.cursorLeft.size() + this.cursorRight.size();
+  }
+}
